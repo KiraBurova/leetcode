019e23f445b6ee98f4f7548955c5b9a08dfca047
Kira Burova <burovakira11@gmail.com>
2022-02-02 19:23:20 +0300

    fixed tests

--- string/LongestCommonPrefix.test.js
+++ string/LongestCommonPrefix.test.js
@@ -1,10 +1,10 @@
 const { expect } = require('@jest/globals');
-const strStr = require('./ImplementStrStr');
+const common = require('./LongestCommonPrefix');
 
 test('return fl', () => {
-  expect(strStr(['flower', 'flow', 'flight'])).toStrictEqual('fl');
+  expect(common(['flower', 'flow', 'flight'])).toStrictEqual('fl');
 });
 
 test('return empty string', () => {
-  expect(strStr(['dog', 'racecar', 'car'])).toStrictEqual('');
+  expect(common(['dog', 'racecar', 'car'])).toStrictEqual('');
 });
